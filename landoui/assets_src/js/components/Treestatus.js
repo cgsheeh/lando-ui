223442947e68dac1c8634531e711b24582fa7e25
Connor Sheehan <cosheehan@mozilla.com>
2023-10-25 11:47:45 -0400

    remove unused treestatus modal modification code
    leave the treestatus.js file since we may need it to change the submission behaviour of the main page's form

--- landoui/assets_src/js/components/Treestatus.js
+++ landoui/assets_src/js/components/Treestatus.js
@@ -9,24 +9,5 @@
 $.fn.treestatus = function() {
     return this.each(function() {
         let $treestatus = $(this);
-
-        // TODO make these things reference Treestatus classes instead.
-        let $modal = $('.Treestatus-modal');
-
-        // Clicking the update tree button causes the form modal to display.
-        let $updateTreeButton = $treestatus.find('.Treestatus-updateTreeButton');
-        $updateTreeButton.on('click', (e) => {
-            // TODO test this
-            // TODO do we need to calculate display state here too?
-            e.preventDefault();
-            $modal.css("display", "flex");
-        });
-
-        // Clicking the close button will remove the form.
-        let $close = $treestatus.find('.StackPage-landingPreview-close');
-        $close.on('click', (e) => {
-            e.preventDefault();
-            $modal.css("display", "none");
-        });
     });
 };
