a75aa43433276841199a0ff5ebc667f96b359aa3
Connor Sheehan <cosheehan@mozilla.com>
2023-11-04 02:08:08 -0400

    make edit/update/etc toggle nicely

--- landoui/assets_src/js/components/Treestatus.js
+++ landoui/assets_src/js/components/Treestatus.js
@@ -23,17 +23,14 @@ $.fn.treestatus = function() {
             targetInput.prop('disabled', !isDisabled);
         });
 
+        // Hide the initially-hidden log update elements.
+        $('.log-update-hidden').toggle();
+
         // Register an on-click handler for each log update edit button.
         $('.log-update-edit').on("click", function () {
-            // Get the `data-target` field for the edit button.
-            let targetId = $(this).data('target');
-
-            // Find the target element.
-            let targetInput = $('#' + targetId);
-
-            // Toggle the `disabled` property on the element.
-            let isDisabled = targetInput.is(':disabled');
-            targetInput.prop('disabled', !isDisabled);
+            // Toggle the elements from hidden/visible.
+            $('.log-update-hidden').toggle();
+            $('.log-update-visible').toggle();
         });
 
         // Toggle selected on all trees.
