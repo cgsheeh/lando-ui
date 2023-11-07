35239afb3409742bd7abd37786106caa6c46efcc
Connor Sheehan <cosheehan@mozilla.com>
2023-11-06 21:19:34 -0500

    hide elements immediately by setting hidden in css

--- landoui/assets_src/js/components/Treestatus.js
+++ landoui/assets_src/js/components/Treestatus.js
@@ -8,12 +8,6 @@
 
 $.fn.treestatus = function() {
     return this.each(function() {
-        let $treestatus = $(this);
-
-        // Hide the initially-hidden log update elements.
-        // TODO this draws in a funky way, how to hide elements before this runs?
-        $('.log-update-hidden').toggle();
-
         // Register an on-click handler for each log update edit button.
         $('.log-update-edit').on("click", function () {
             // Toggle the elements from hidden/visible.
@@ -22,10 +16,7 @@ $.fn.treestatus = function() {
             closest_form.find('.log-update-visible').toggle();
         });
 
-        // Hide the initially-hidden changes update elements.
-        // TODO hide elements in CSS instead of here in JS.
-        $('.recent-changes-update-hidden').toggle();
-
+        // Register an on-click handler for each recent changes edit button.
         $('.recent-changes-edit').on("click", function () {
             // Toggle the elements from hidden/visible.
             var closest_form = $(this).closest('.recent-changes-form');
